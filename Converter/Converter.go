4c62e170e9c55575e549bda5ed54ddbaf6392f7d
IAMBURNINGMAN <nikitadogolev0396@gmail.com>
2025-11-05 00:15:53 +0300

    DELETE+PATCH

--- Converter/Converter.go
+++
@@ -1,9 +0,0 @@
-package Converter
-
-func CelsiusToFahrenheit(c float64) float64 {
-	return (c * 9 / 5) + 32
-}
-
-func KilometersToMiles(k float64) float64 {
-	return k * 0.62
-}
--- main.go
+++ main.go
@@ -1,47 +1,78 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"sync"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type TaskReq struct {
-	Task string `json:"task"`
-}
-
-type TaskRes struct {
-	Task string `json:"task"`
+type Task struct {
+	Task      string `json:"task"`
+	ID        string `json:"id"`
+	Completed string `json:"completed"`
 }
 
 var (
-	lastTask string
-	mu       sync.RWMutex
+	mu sync.RWMutex
 )
 
+var TASKLIST = []Task{}
+
 func PostHandler(c echo.Context) error {
-	var req TaskReq
+	var req Task
 	if err := c.Bind(&req); err != nil {
 		c.Logger().Errorf("bind error: %v", err)
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "bind error"})
 	}
 	mu.Lock()
-	lastTask = req.Task
+	task := Task{req.Task, uuid.NewString(), req.Completed}
+	TASKLIST = append(TASKLIST, task)
 	mu.Unlock()
-	return c.JSON(http.StatusOK, TaskRes{Task: lastTask})
+	return c.JSON(http.StatusOK, Task{Task: req.Task, ID: req.ID, Completed: req.Completed})
+}
+
+func PatchHandler(c echo.Context) error {
+	idpatcher := c.Param("id")
+	var req Task
+	if err := c.Bind(&req); err != nil {
+		c.Logger().Errorf("bind error: %v", err)
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "bind error"})
+	}
+	for i, tasking := range TASKLIST {
+		if tasking.ID == idpatcher {
+			TASKLIST[i].Completed = req.Completed
+			TASKLIST[i].Task = req.Task
+			return c.JSON(http.StatusOK, TASKLIST[i])
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Task not found"})
 }
 
 func GetHandler(c echo.Context) error {
-	mu.RLock()
-	t := lastTask
-	mu.RUnlock()
-	if t == "" {
+	if len(TASKLIST) == 0 {
 		return c.JSON(http.StatusNotFound, map[string]string{"error": "no task set"})
 	}
-	return c.JSON(http.StatusOK, fmt.Sprintf("привет, %s", t))
+	mu.RLock()
+	t := TASKLIST[0]
+	mu.RUnlock()
+	if len(TASKLIST) == 0 {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "no task set"})
+	}
+	return c.JSON(http.StatusOK, t)
+}
+
+func deleteHandler(c echo.Context) error {
+	id := c.Param("id")
+	for i, t := range TASKLIST {
+		if t.ID == id {
+			TASKLIST = append(TASKLIST[:i], TASKLIST[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]string{"error": "Task not found"})
 }
 
 func main() {
@@ -51,6 +82,8 @@ func main() {
 
 	e.GET("/task", GetHandler)
 	e.POST("/task", PostHandler)
+	e.PATCH("/task/:id", PatchHandler)
+	e.DELETE("/task/:id", deleteHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
